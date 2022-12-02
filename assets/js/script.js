777618191b93f8501768aa1535e9d5980cf58c8c
Marwin Manlangit <marwin.m@hotmail.com>
2022-12-02 16:24:56 +1100

    Finihsed basic html structure, and array for questions

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+var questions = [
+    {
+        question: "Singe line comments start with:",
+        choiceOne: "<>",
+        choiceTwo: "//",
+        choiceThree: "\\",
+        choiceFour: "<!-->",
+        answer: 2
+    },
+    {
+        question: "What is used to declare a JavaScript Value?",
+        choiceOne: "var",
+        choiceTwo: "variable",
+        choiceThree: "iable",
+        choiceFour: "function",
+        answer: 1
+    },
+    {
+    question: "If a variable is decleared with no value, what happens?",
+        choiceOne: "Nothing will happen",
+        choiceTwo: "0 will pop up",
+        choiceThree: "Variable will have an undefined value",
+        choiceFour: "A change within HTML will occur",
+        answer: 3
+    },
+    {
+        question: "What does + mean in JavaScript?",
+        choiceOne: "Multiplication",
+        choiceTwo: "Increment",
+        choiceThree: "Subtraction",
+        choiceFour: "Addition",
+        answer: 4
+    },
+    {
+        question: "What does ++ mean in JavaScript?",
+        choiceOne: "Decrement",
+        choiceTwo: "Increment",
+        choiceThree: "Subtraction",
+        choiceFour: "Addition",
+        answer: 2
+    },
+    {
+        question: "What does * mean in JavaScript?",
+        choiceOne: "Multiplication",
+        choiceTwo: "Increment",
+        choiceThree: "Subtraction",
+        choiceFour: "Division",
+        answer: 1
+    },
+    {
+        question: "What does + mean in JavaScript?",
+        choiceOne: "Multiplication",
+        choiceTwo: "Increment",
+        choiceThree: "Subtraction",
+        choiceFour: "Addition",
+        answer: 4
+    },
+    {
+        question: "How do we find out the length of an array?",
+        choiceOne: ".length",
+        choiceTwo: ".width",
+        choiceThree: "length.size",
+        choiceFour: "size.array",
+        answer: 1
+    },
+    {
+        question: "Where does <script> go?",
+        choiceOne: "Within README",
+        choiceTwo: "Anywhere in CSS",
+        choiceThree: "Body of HTML",
+        choiceFour: "Inside script.js",
+        answer: 3
+    },
+    {
+        question: "What operators do we use to store within arrays?",
+        choiceOne: "!!",
+        choiceTwo: "//",
+        choiceThree: "{}",
+        choiceFour: "[]",
+        answer: 4
+    }
+]
